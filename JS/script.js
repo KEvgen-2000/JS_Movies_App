cf5bc373eb5438d07b32eefca7d9f779c85187e6
--local <--local>
2021-05-10 17:36:22 +0300

    lesson_2_code

--- JS/script.js
+++ JS/script.js
@@ -26,7 +26,8 @@
 
 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
 отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+возвращаем пользователя к вопросам опять (к любой строке можно обратиться как 
+    str.length - иполучить ее длину)
 
 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
 "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
@@ -47,13 +48,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i<2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+   		  b = prompt('На сколько оцените его?', ''); 
 
-console.log(personalMovieDB);
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('Done');
+		}  else {
+			console.log('Erorr');
+			i--;
+		}
+    
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
+
+ console.log(personalMovieDB);
 
